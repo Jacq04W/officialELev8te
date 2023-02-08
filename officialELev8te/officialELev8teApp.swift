c8e9bfc737428c9c7d7870e00fcc9cbe9c8c9644
Jacquese <jacqwhitson@gmail.com>
2023-02-08 18:40:10 -0500

    Initial Commit

---
+++ officialELev8te/officialELev8teApp.swift
@@ -0,0 +1,17 @@
+//
+//  officialELev8teApp.swift
+//  officialELev8te
+//
+//  Created by Jacquese Whitson  on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct officialELev8teApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
